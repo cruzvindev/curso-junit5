155c69d198504841702559f8fb87e820f06f438a
Vinicius Cruz <cruzvindev@gmail.com>
2025-01-01 10:53:49 -0300

    Utilizando @Nested para organizar testes

--- src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/ContaBancariaTest.java
@@ -1,5 +1,6 @@
 package com.algaworks.junit.utilidade;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -9,88 +10,94 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ContaBancariaTest {
 
-    @Test
-    void deveLancarExcecaoAoPassarNullParaConstrutor(){
-         assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(null));
-    }
+    @Nested
+    class Saque {
+        @Test
+        void deveSacarAoForncerValorValido(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            contaBancaria.saque(BigDecimal.valueOf(50));
+            assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(100).subtract(contaBancaria.saldo()));
+        }
 
-    @Test
-    void deveAdicionarSaldoAoCriarContaComSaldoPositivo(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(100));
-    }
+        @Test
+        void deveLancarExcecaoAoPassarZeroParaSacar(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(ZERO));
+        }
 
-    @Test
-    void deveAdicionarSaldoAoCriarContaComSaldoNegativo(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(-100));
-        assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(-100));
-    }
+        @Test
+        void deveLancarExcecaoAoPassarNullParaSacar(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(null));
+        }
 
-    @Test
-    void deveAdicionarSaldoAoCriarContaComSaldoZero(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(0));
-        assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(0));
-    }
+        @Test
+        void deveLancarExcecaoAoPassarValorNegativoParaSacar(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(BigDecimal.valueOf(-10)));
+        }
 
-    @Test
-    void deveSacarAoForncerValorValido(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        contaBancaria.saque(BigDecimal.valueOf(50));
-        assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(100).subtract(contaBancaria.saldo()));
+        @Test
+        void deveLancarExcecaoQuandoSaldoInsuficiente(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
+            assertThrows(RuntimeException.class, () -> contaBancaria.saque(BigDecimal.valueOf(100)));
+        }
     }
+    @Nested
+    class Deposito {
+        @Test
+        void deveDepositarAoForncerValorValido(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            contaBancaria.deposito(BigDecimal.valueOf(200));
+            assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(300));
+        }
 
-    @Test
-    void deveLancarExcecaoAoPassarZeroParaSacar(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(ZERO));
+        @Test
+        void deveLancarExcecaoAoPassarNullParaDepositar(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(null));
+        }
+
+        @Test
+        void deveLancarExcecaoAoPassarZeroParaDepositar(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(ZERO));
+        }
+
+        @Test
+        void deveLancarExcecaoAoPassarNegativoParaDepositar(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(BigDecimal.valueOf(-30)));
+        }
     }
+    @Nested
+    class Saldo{
+        @Test
+        void deveLancarExcecaoAoPassarNullParaConstrutor(){
+            assertThrows(IllegalArgumentException.class, () -> new ContaBancaria(null));
+        }
 
-    @Test
-    void deveLancarExcecaoAoPassarNullParaSacar(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(null));
-    }
+        @Test
+        void deveAdicionarSaldoAoCriarContaComSaldoPositivo(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(100));
+        }
 
-    @Test
-    void deveLancarExcecaoAoPassarValorNegativoParaSacar(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        assertThrows(IllegalArgumentException.class, () -> contaBancaria.saque(BigDecimal.valueOf(-10)));
-    }
+        @Test
+        void deveAdicionarSaldoAoCriarContaComSaldoNegativo(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(-100));
+            assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(-100));
+        }
 
-    @Test
-    void deveLancarExcecaoQuandoSaldoInsuficiente(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(50));
-        assertThrows(RuntimeException.class, () -> contaBancaria.saque(BigDecimal.valueOf(100)));
-    }
-
-    @Test
-    void deveDepositarAoForncerValorValido(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        contaBancaria.deposito(BigDecimal.valueOf(200));
-        assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(300));
-    }
-
-    @Test
-    void deveLancarExcecaoAoPassarNullParaDepositar(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(null));
-    }
-
-    @Test
-    void deveLancarExcecaoAoPassarZeroParaDepositar(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(ZERO));
-    }
-
-    @Test
-    void deveLancarExcecaoAoPassarNegativoParaDepositar(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        assertThrows(IllegalArgumentException.class, () -> contaBancaria.deposito(BigDecimal.valueOf(-30)));
-    }
-
-    @Test
-    void deveRetornarSaldo(){
-        ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
-        assertNotNull(contaBancaria.saldo());
+        @Test
+        void deveAdicionarSaldoAoCriarContaComSaldoZero(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(0));
+            assertEquals(contaBancaria.saldo(), BigDecimal.valueOf(0));
+        }
+        @Test
+        void deveRetornarSaldo(){
+            ContaBancaria contaBancaria = new ContaBancaria(BigDecimal.valueOf(100));
+            assertNotNull(contaBancaria.saldo());
+        }
     }
 }
--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -12,13 +12,23 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class FiltroNumerosTest {
 
+//    @Test
+//    public void deve_Retornar_Numeros_Pares(){
+//        List<Integer> numeros = Arrays.asList(1,2,3,4);
+//        List<Integer> numerosParesEsperados = Arrays.asList(2,4);
+//        List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
+//        //Assertions.assertArrayEquals(numerosParesEsperados.toArray(new Object[]{}), resultadoFiltro.toArray(new Object[]{}));
+//        assertIterableEquals(numerosParesEsperados, resultadoFiltro);
+//    }
+
+    //Given When Then
     @Test
-    public void deve_Retornar_Numeros_Pares(){
+    public void Dado_uma_lista_de_numeros_Quando_filtrar_por_impares_Então_deve_retornar_apenas_numeros_impares(){
         List<Integer> numeros = Arrays.asList(1,2,3,4);
-        List<Integer> numerosParesEsperados = Arrays.asList(2,4);
-        List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
+        List<Integer> numerosImparesEsperados = Arrays.asList(1,3);
+        List<Integer> resultadoFiltro = FiltroNumeros.numerosImpares(numeros);
         //Assertions.assertArrayEquals(numerosParesEsperados.toArray(new Object[]{}), resultadoFiltro.toArray(new Object[]{}));
-        assertIterableEquals(numerosParesEsperados, resultadoFiltro);
+        assertIterableEquals(numerosImparesEsperados, resultadoFiltro);
     }
 
 }
