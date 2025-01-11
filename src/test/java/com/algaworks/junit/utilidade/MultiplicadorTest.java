374b2bfe2485553bb98c16583189ed543bb97fff
Vinicius Cruz <cruzvindev@gmail.com>
2025-01-11 13:46:24 -0300

    Adicionando Jacoco ao projeto para obter relatório de cobertura de testes

--- src/test/java/com/algaworks/junit/utilidade/MultiplicadorTest.java
+++ src/test/java/com/algaworks/junit/utilidade/MultiplicadorTest.java
@@ -1,6 +1,5 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
